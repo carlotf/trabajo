fb9d9979c57b24ccab23f23ad7500b19286f2da8
Andrés Becerra <61631134+carlotf@users.noreply.github.com>
2020-03-12 09:42:02 -0500

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public int DevAcotado(String cadena)
+        {
+            cs_loquito LQ = new cs_loquito();
+            return LQ.loquito(cadena);
+        }
+    }
+}
---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class cs_loquito
+    {
+        int cantidad;
+
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+
+
+        public int loquito(string numeros)
+        {
+            return 0;
+        }
+    }
+}
